6f9888b27a7c20c23eec5a6e43a2e396da3a9c64
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-01-15 09:03:58 -0700

    Updated it.

---
+++ src/list_write.cpp
@@ -0,0 +1,33 @@
+#include "list_read.h"
+#include "list_write.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void listRelease(LIST *pLIST){
+    fprintf(stderr, "You need to write the function listRelease\n");
+}
+
+
+NODE * listInsert(LIST *pLIST, double key){
+    NODE *pNODE;
+    pNODE = NULL;
+    fprintf(stderr, "You need to write the function listInsert\n");
+    return pNODE;
+}
+
+
+NODE * listAppend(LIST *pLIST, double key){
+    NODE *pNODE;
+    pNODE = NULL;
+    fprintf(stderr, "You need to write the function listAppend\n");
+    return pNODE;
+}
+
+
+NODE * listDelete(LIST *pLIST, double key){
+    NODE *pNODE;
+    pNODE = NULL;
+    fprintf(stderr, "You need to write the function listDelete\n");
+    return pNODE;
+}
+
