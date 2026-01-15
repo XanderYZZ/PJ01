7ff6c845221ab3bbf4654a042dd4b8d5147aca04
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-01-15 09:55:09 -0700

    Updated two functions.

--- src/list_write.cpp
+++ src/list_write.cpp
@@ -4,22 +4,42 @@
 #include <stdlib.h>
 
 void listRelease(LIST *pLIST){
-    fprintf(stderr, "You need to write the function listRelease\n");
+    NODE *curr = pLIST->head;
+
+    while (curr != NULL) {
+        NODE *temp = curr;
+        curr = curr->next;
+        free(temp); 
+    }
+
+    pLIST->head = NULL;
+    pLIST->tail = NULL;
+    pLIST->length = 0;
 }
 
-
+// Inserting at the head.
 NODE * listInsert(LIST *pLIST, double key){
-    NODE *pNODE;
-    pNODE = NULL;
-    fprintf(stderr, "You need to write the function listInsert\n");
+    NODE *pNODE = (NODE *) malloc(sizeof(NODE));
+    if (!pNODE) { return NULL; }
+
+    pNODE->key = key;
+    pNODE->next = pLIST->head;
+    pLIST->head = pNODE;
+
+    if (pLIST->tail == NULL) {
+        pLIST->tail = pNODE;
+    }
+
+    pLIST->length += 1;
+
     return pNODE;
 }
 
-
+// Inserting at the tail.
 NODE * listAppend(LIST *pLIST, double key){
     NODE *pNODE;
-    pNODE = NULL;
-    fprintf(stderr, "You need to write the function listAppend\n");
+
+    
     return pNODE;
 }
 
